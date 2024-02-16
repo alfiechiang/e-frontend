ad6f641bb241ffca7380bf8ccb16a7c033f3754f
zack <zack_apollo_t0817@ie8.com>
2024-02-17 01:26:46 +0800

    update

--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -5,8 +5,10 @@
       <div class="first-layer">
         <div class="left-header">
           <img :src="imageSrc" alt="描述">
-          <span>回到從錢</span> 
-          <el-icon color="#36BAA0" size="25px"><LocationFilled /></el-icon>
+          <span>回到從錢</span>
+          <el-icon color="#36BAA0" size="25px">
+            <LocationFilled />
+          </el-icon>
         </div>
         <div class="right-header">
           <span>登入／註冊</span>
@@ -21,7 +23,7 @@
       </div>
       <div class="second-layer">
         <ul>
-          <li><span>商品</span></li>
+          <li><span class="product-font">商品</span></li>
           <li><span>送貨方式</span></li>
           <li><span>付款方式</span></li>
           <li><span>關於我們</span></li>
@@ -29,21 +31,20 @@
         </ul>
 
         <div class="icon-area">
-          <el-icon size="20px" ><Search /></el-icon>
-        <img :src="shareIcon" alt="">
+          <el-icon size="20px">
+            <Search />
+          </el-icon>
+          <img :src="shareIcon" alt="">
         </div>
-       
+
       </div>
 
 
     </header>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
-
-
-
-
-
-  <!-- <router-view></router-view> -->
 </template>
   
 <script setup lang="ts">
@@ -61,6 +62,11 @@ import shareIcon from '@/assets/share.png';
   height: 100%;
   margin: 0 auto;
 
+  .content{
+    width: 100%;
+    height: 100%;
+  }
+
   header {
     width: 100%;
     height: 150px;
@@ -77,11 +83,13 @@ import shareIcon from '@/assets/share.png';
         height: 100%;
         display: flex;
         align-items: center;
-        img{
+
+        img {
           max-height: 50px;
           margin-right: 20px;
         }
-        span{
+
+        span {
           margin-right: 20px;
           font-size: 30px;
           font-weight: 500;
@@ -95,17 +103,17 @@ import shareIcon from '@/assets/share.png';
         align-items: center;
         justify-content: end;
 
-        .shopping-icon{
+        .shopping-icon {
           cursor: pointer;
         }
 
-        button:hover{
+        button:hover {
           background: #EDEDED;
           border: 1px solid #D2D2D2;
         }
 
 
-        button{
+        button {
           width: 125px;
           height: 35px;
           background: #fff;
@@ -115,14 +123,16 @@ import shareIcon from '@/assets/share.png';
           align-items: center;
           padding: 5px;
           cursor: pointer;
-          .currency-icon{
+
+          .currency-icon {
             margin-right: 5px;
             background-image: url('../../assets/currency-icon/taiwan.png');
             height: 20px;
             width: 32px;
             background-size: cover;
           }
-          .currency-unit{
+
+          .currency-unit {
             border-right: 1px solid #EDEDED;
             margin-right: 12px;
             width: 40px;
@@ -133,7 +143,8 @@ import shareIcon from '@/assets/share.png';
           }
 
         }
-        img{
+
+        img {
           max-height: 22px;
           margin-left: 15px;
         }
@@ -142,7 +153,7 @@ import shareIcon from '@/assets/share.png';
 
     }
 
-    .second-layer{
+    .second-layer {
       display: flex;
       align-items: center;
       width: 100%;
@@ -151,7 +162,7 @@ import shareIcon from '@/assets/share.png';
       border-bottom: 1px solid #E7E7E7;
 
 
-      ul{
+      ul {
         display: flex;
         width: 600px;
         height: 50px;
@@ -159,14 +170,25 @@ import shareIcon from '@/assets/share.png';
         align-items: center;
         cursor: pointer;
 
-        span{
+        .product-font::after {
+          content: '';
+          display: inline-block;
+          width: 10px;
+          height: 10px;
+          background-image: url('../../assets/arrow-down.png');
+          background-size: cover;
+          margin-left: 5px; /* 调整左边距 */
+
+        }
+
+        span {
           font-size: 16px;
           font-weight: 500;
           color: #222222;
         }
       }
 
-      .icon-area{
+      .icon-area {
         display: flex;
         align-items: center;
         justify-content: space-around;
@@ -175,9 +197,6 @@ import shareIcon from '@/assets/share.png';
         width: 110px;
       }
 
-
-
-
       img {
         max-width: 16px;
       }
@@ -185,6 +204,5 @@ import shareIcon from '@/assets/share.png';
 
   }
 
-}
-</style>
+}</style>
   
--- src/views/hall/index.vue
+++ src/views/hall/index.vue
@@ -1,7 +1,37 @@
 <template>
-    <h1>vdvd</h1>
+    <div id="hall">
+        <div class="banner">
+            <img :src="bannerSrc" alt="">
+            <div class="second-layer">
+                <span>本小店主要銷售中國歷代古泉銀銅器什件，亦兼售各國機製幣以供選購</span>
+                <div class="icon-area">
+                    <div class="ig-icon-area"></div>
+                    <div class="fb-icon-area"></div>
+                    <div class="star-icon-area"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
     
-    <script setup lang="ts">
-    </script>
+<script setup lang="ts">
+
+import bannerSrc from '@/assets/banner.jpg';
+
+</script>
+  
+<style lang="scss" scoped>
+#hall {
+    .banner {
+        width: 100%;
+        height: 600px;
+        img{
+            max-width: 100%;
+        }
+    }
+
+
+
+}
+</style>
     
